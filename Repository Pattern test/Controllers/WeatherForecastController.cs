a1a8745244a4b5b8671b8bd0498953db6a32d895
johannpa <johann_paque@hotmail.fr>
2023-10-19 23:50:31 +0200

    Add the implementation to use, register the service in Program.cs

--- Repository Pattern test/Controllers/WeatherForecastController.cs
+++ Repository Pattern test/Controllers/WeatherForecastController.cs
@@ -20,13 +20,7 @@ namespace Repository_Pattern_test.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return _weatherForecastService.Get();
         }
     }
 }
